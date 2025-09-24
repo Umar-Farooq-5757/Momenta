272afce12792d4a76f3a45d85f06890a2440a219
Umar-Farooq-5757 <aamirgel17@gmail.com>
2025-09-24 16:25:43 +0500

    Some updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,45 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
-const connectDB = async () => {
-  if (global._mongoClientPromise) {
-    return global._mongoClientPromise;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI environment variable is required.");
+}
+
+const globalWithMongoose = globalThis;
+
+if (!globalWithMongoose._mongoCache) {
+  globalWithMongoose._mongoCache = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (globalWithMongoose._mongoCache.conn) {
+    return globalWithMongoose._mongoCache.conn;
   }
-  if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI not set");
 
-  global._mongoClientPromise = mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  if (!globalWithMongoose._mongoCache.promise) {
+    globalWithMongoose._mongoCache.promise = mongoose
+      .connect(MONGODB_URI, {})
+      .then((mongooseInstance) => {
+        return mongooseInstance.connection;
+      })
+      .catch((err) => {
+        globalWithMongoose._mongoCache.promise = null;
+        throw err;
+      });
+  }
 
-  return global._mongoClientPromise;
-};
+  try {
+    const conn = await globalWithMongoose._mongoCache.promise;
+    globalWithMongoose._mongoCache.conn = conn;
+    console.log("[db] mongoose connected:", conn.host || "[unknown-host]");
+    return conn;
+  } catch (err) {
+    console.error("[db] mongoose connection error:", err);
+    throw err;
+  }
+}
 
 export default connectDB;
